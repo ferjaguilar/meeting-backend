31a981a91bbd00d795aa824a4b726226135d0a46
Fernando Aguilar <ferrivas127@gmail.com>
2021-05-09 21:04:31 -0600

    Create signup mutation and encrypt password

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,5 +1,21 @@
+import { ApolloError } from 'apollo-server';
+import bcrypt from 'bcrypt';
+import userModel from '../models/userModel';
+
 exports.resolver = {
   Query: {
     hello: () => 'world',
+  },
+  Mutation: {
+    signup: async (__:void, { input }: any) => {
+      // eslint-disable-next-line no-param-reassign
+      input.password = bcrypt.hashSync(input.password, 10);
+      const findEmail = await userModel.findOne({ email: input.email });
+      if (findEmail) {
+        throw new ApolloError('Email already registered');
+      }
+      const userDB = await userModel.create(input);
+      return userDB;
+    },
   },
 };
