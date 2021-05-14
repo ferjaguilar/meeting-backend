d9c562b156ba4192449ee6247ba7b8f2bf00e86b
Fernando Aguilar <ferrivas127@gmail.com>
2021-05-13 18:26:14 -0600

    Remake students to studentControllers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,18 +3,13 @@ import { Imeeting } from '../Interfaces/Imeeting';
 import { Iuser, IuserContext } from '../Interfaces/Iusers';
 import meetingModel from '../models/meetingModel';
 import studentModel from '../models/studentModel';
-import userController from '../controllers/userControllers';
+import userControllers from '../controllers/userControllers';
+import studentControllers from '../controllers/studentControllers';
 
 exports.resolver = {
   Query: {
-    login: async (__:void, { input }:{input:Iuser}) => userController.login(input),
-    getStudent: async (__:void, { code }:{ code:string }) => {
-      const studentDB = await studentModel.findOne({ code });
-      if (studentDB?.completed) {
-        throw new ApolloError('Student has already registered', 'NOT_FOUND');
-      }
-      return studentDB;
-    },
+    login: async (__:void, { input }:{input:Iuser}) => userControllers.login(input),
+    getStudent: async (__:void, { code }:{ code:string }) => studentControllers.getStudent(code),
     // eslint-disable-next-line max-len
     getMeetings: async (__:void, { limit, skip }:{limit:number, skip:number}, { user }:{user: IuserContext}) => {
       if (!user) throw new AuthenticationError('You must be logged in');
@@ -50,7 +45,7 @@ exports.resolver = {
   },
   Mutation: {
     // eslint-disable-next-line max-len
-    signup: async (__:void, { input }:{input:Iuser}, { user }:{user: IuserContext}) => userController.signup(input, user),
+    signup: async (__:void, { input }:{input:Iuser}, { user }:{user: IuserContext}) => userControllers.signup(input, user),
     addMeeting: async (__:void, { input }:{input:Imeeting}) => {
       const findMeeting = await meetingModel.findOne({ studentId: input.studentId });
       if (findMeeting) throw new ApolloError('This student already registered');
