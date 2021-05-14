d81db4943bca0c603c63fdc3b865be45cd04dcdd
Fernando Aguilar <ferrivas127@gmail.com>
2021-05-13 18:19:46 -0600

    Remake code to userControllers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,23 +1,13 @@
 import { ApolloError, AuthenticationError } from 'apollo-server';
-import bcrypt from 'bcrypt';
 import { Imeeting } from '../Interfaces/Imeeting';
 import { Iuser, IuserContext } from '../Interfaces/Iusers';
 import meetingModel from '../models/meetingModel';
 import studentModel from '../models/studentModel';
-import userModel from '../models/userModel';
-import { generateToken } from '../utils/tokenUtils';
+import userController from '../controllers/userControllers';
 
 exports.resolver = {
   Query: {
-    login: async (__:void, { input }:{input:Iuser}) => {
-      const findUserInformation = await userModel.findOne({ email: input.email });
-      if (!findUserInformation) throw new ApolloError('Email! or password not found');
-      if (!bcrypt.compareSync(input.password, findUserInformation.password)) throw new ApolloError('Email! or password not found');
-
-      return {
-        token: generateToken(findUserInformation, String(process.env.SECRET), '24h'),
-      };
-    },
+    login: async (__:void, { input }:{input:Iuser}) => userController.login(input),
     getStudent: async (__:void, { code }:{ code:string }) => {
       const studentDB = await studentModel.findOne({ code });
       if (studentDB?.completed) {
@@ -59,17 +49,8 @@ exports.resolver = {
     },
   },
   Mutation: {
-    signup: async (__:void, { input }:{input:Iuser}, { user }:{user: IuserContext}) => {
-      if (!user) throw new AuthenticationError('You must be logged in');
-      // eslint-disable-next-line no-param-reassign
-      input.password = bcrypt.hashSync(input.password, 10);
-      const findEmail = await userModel.findOne({ email: input.email });
-      if (findEmail) {
-        throw new ApolloError('Email already registered');
-      }
-      const userDB = await userModel.create(input);
-      return userDB;
-    },
+    // eslint-disable-next-line max-len
+    signup: async (__:void, { input }:{input:Iuser}, { user }:{user: IuserContext}) => userController.signup(input, user),
     addMeeting: async (__:void, { input }:{input:Imeeting}) => {
       const findMeeting = await meetingModel.findOne({ studentId: input.studentId });
       if (findMeeting) throw new ApolloError('This student already registered');
